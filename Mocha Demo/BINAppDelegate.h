7cffb3d14b46220fc9dc6f4fbe2669729e446896
Aditya Vaidyam <aditya@fluffy-the-mac.local>
2013-07-25 15:57:35 -0700

    A bit of refactoring...

---
+++ Mocha Demo/BINAppDelegate.h
@@ -0,0 +1,14 @@
+/*
+ *  Mocha.framework
+ *
+ *  Copyright (c) 2013 Galaxas0. All rights reserved.
+ *  For more copyright and licensing information, please see LICENSE.md.
+ */
+
+#import <Mocha/Mocha.h>
+
+@interface BINAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (nonatomic, strong) IBOutlet NSWindow *window;
+
+@end
---
+++ Mocha Demo/BINAppDelegate.m
@@ -0,0 +1,16 @@
+/*
+ *  Mocha.framework
+ *
+ *  Copyright (c) 2013 Galaxas0. All rights reserved.
+ *  For more copyright and licensing information, please see LICENSE.md.
+ */
+
+#import "BINAppDelegate.h"
+
+@implementation BINAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+	
+}
+
+@end
