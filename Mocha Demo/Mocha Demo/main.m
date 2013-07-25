deb1ffcdd770ef9d1e55b76ca1104adb56344949
Aditya Vaidyam <aditya@fluffy-the-mac.local>
2013-07-25 15:55:50 -0700

    Add a demo project - still needs some work.

---
+++ Mocha Demo/Mocha Demo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mocha Demo
+//
+//  Created by Aditya Vaidyam on 7/25/13.
+//  Copyright (c) 2013 Aditya Vaidyam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
